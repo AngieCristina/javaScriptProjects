78b2ef6edf21cac279ab36791e311dbe4ab5776a
Angie Cristina <angiecristinagonzalessarae@gmail.com>
2020-07-14 13:03:05 -0500

    To remember how works some methods

--- loops.js
+++ loops.js
@@ -38,3 +38,26 @@ const startsWithS = animals.findIndex(letter =>
   return letter[0]=== "s"
 })
 console.log(startsWithS)
+//practice with methods
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.some(num => num < 0);
